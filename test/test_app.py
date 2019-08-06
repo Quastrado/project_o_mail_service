f89bb38c9d6749b5705ab1330ab7397d14cf2aa3
Quastrado <you@example.com>
2019-08-06 15:07:10 +0300

    test initializing the database

--- test/test_app.py
+++ test/test_app.py
@@ -1,6 +1,9 @@
 import pytest
 
+from flask_sqlalchemy import SQLAlchemy
+
 from owl_mail import create_app
+from owl_mail.models import User
 
 @pytest.fixture
 def app():
@@ -11,3 +14,17 @@ def app():
 def test_app(client):
     response = client.get('/login')
     assert response.status_code == 200
+
+
+@pytest.fixture(scope='module')
+def init_database():
+    db.create_all()
+
+    user1 = User('Ghost', 'invisible', 'admin')
+    user2 = User('Vagabound', 'danger', 'user')
+    db.session.add(user1)
+    db.session.add(user2)
+
+    yield db
+
+    db.drop_all() 
